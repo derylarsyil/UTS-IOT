62ee1cd586ffc40b7fdacf2c72455246bb344923
derylarsyil <derilarsilarz@gmail.com>
2024-11-13 21:08:14 +0700

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
+
+// Menggunakan middleware CORS
+app.use(cors());
 
 // Data JSON yang akan dikirimkan
 const data = {
     suhuMax: 36,
     suhuMin: 25,
     suhuRata: 28.5,
+    kelembapan: 60,  // Menambahkan data kelembapan
+    kekeruhan: 12,  // Menambahkan data kekeruhan
+    ph: 6.5,        // Menambahkan data pH
     nilai_suhu_max_humid_max_min: [
         { idx: 162, suhu: 36, kelembaban: 27, kecerahan: 25, timestamp: "2018-09-18 07:23:49" },
         { idx: 205, suhu: 35, kelembaban: 27, kecerahan: 25, timestamp: "2019-05-12 09:14:14" }
